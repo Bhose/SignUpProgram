d8ea45eb5128e5e0935487f2b080285a16d9efce
Brittney Hose <bhose10@gmail.com>
2017-11-29 20:42:13 -0500

    all lowercase password is invalid, fixed uppercase code

--- user.rb
+++ user.rb
@@ -20,16 +20,38 @@ def set_up_email(email, reenter_email)
 end
 
 def set_up_password(password)
+	indicator = 0
+	includesCaps = false
+
+	upcase = [*('A'..'Z')]
+	# lowcase = [a..z]
+	# number = [0..9]
+	# spechar = ["!", "@", "#", "$", "%", "^", "&", "*"]
 	password = password.to_s
+
 	if password.length > 7
 		"valid"
 	else
 		"invalid"
 	end
 
-	if password =~ /[Password]/
-		"valid"
-	else
-		"invalid"
+	upcase.each do |letter|
+		if password.include?(letter)
+			 includesCaps = true
+		end
 	end
+
+	if includesCaps == true
+		return "valid"
+	else 
+		return "invalid"
+	end
+
+
+# if password =~ /[Password]/#will this work for any password with caps?
+	# 	"valid"
+	# end
+	# if password =~ /password/
+	# 	"invalid"
+	# end
 end
--- usertest.rb
+++ usertest.rb
@@ -43,19 +43,24 @@ class TestUser < Minitest::Test
 	# 	assert_equal(String, set_up_password(password))
 	# end
 
-	def test_assert_that_password_is_8_characters
-		password = "password"
-		assert_equal("valid", set_up_password(password))
-	end
+	# def test_assert_that_password_is_8_characters
+	# 	password = "password"
+	# 	assert_equal("valid", set_up_password(password))
+	# end
 
-	def test_assert_that_password_with_5_chars_is_invalid
-		password = "hi"
-		assert_equal("invalid", set_up_password(password))
-	end
+	# def test_assert_that_password_with_5_chars_is_invalid
+	# 	password = "hi"
+	# 	assert_equal("invalid", set_up_password(password))
+	# end
 
-	def test_assert_that_password_contains_uppercase_letter
+	def test_assert_that_password_contains_uppercase_letters
 		password = "Password"
 		assert_equal("valid", set_up_password(password))
+	end
+
+	def test_assert_that_password_with_no_uppercase_is_invalid
+		password = "lowercase"
+		assert_equal("invalid", set_up_password(password))
 	end
 
 end
